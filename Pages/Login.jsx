d2fa1f4d5f11ce9e95b7828e4783241e9002cf5d
Irit41 <81565397+Irit41@users.noreply.github.com>
2022-06-02 15:13:50 +0300

    v2

--- Pages/Login.jsx
+++ Pages/Login.jsx
@@ -1,10 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 
-export default function Login({navigation}) {
+export default function Login({ navigation }) {
   return (
     <View>
-      <Text>Login</Text>
+      <Text style={styles.HeadLine}>Login</Text>
+      <Text style={styles.HeadLine}>Email</Text>
+      <Text style={styles.HeadLine}>Password</Text>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  HeadLine: {
+    fontSize: 40,
+
+    fontWeight: "bold",
+    paddingTop: 20,
+    alignItems: "center",
+    textAlign: "center",
+    justifyContent: "center",
+  },
+});
--- Pages/Navbar.jsx
+++ Pages/Navbar.jsx
@@ -6,11 +6,12 @@ export default function Navbar({navigation}) {
     return (
         <View>
             <Button title='Homepage' onPress={()=> navigation.navigate('Homepage')}></Button>
-            <Button title='Booking' onPress={()=> navigation.navigate('Booking')}></Button>
+          
             <Button title='Login' onPress={()=> navigation.navigate('Login')}></Button>
-            <Button title='Payment' onPress={()=> navigation.navigate('Payment')}></Button>
+            {/* <Button title='Payment' onPress={()=> navigation.navigate('Payment')}></Button> */}
             <Button title='Registration' onPress={()=> navigation.navigate('Registration')}></Button>
-            <Button title='SaveRoom' onPress={()=> navigation.navigate('SaveRoom')}></Button>
+            {/* <Button title='SaveRoom' onPress={()=> navigation.navigate('SaveRoom')}></Button> */}
+            <Button title='Booking' onPress={()=> navigation.navigate('Booking')}></Button>
         </View>
     )
 }
