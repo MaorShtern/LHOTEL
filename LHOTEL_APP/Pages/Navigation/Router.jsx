88fdaf695369f61eb44fce51fb66a39b1a95a010
Irit41 <81565397+Irit41@users.noreply.github.com>
2022-09-05 12:21:32 +0300

    05/09

--- LHOTEL_APP/Pages/Navigation/Router.jsx
+++ LHOTEL_APP/Pages/Navigation/Router.jsx
@@ -2,46 +2,42 @@ import React from 'react'
 import Home from '../Home';
 import Drawer from './Drawer';
 import Tasks from '../Workers/Tasks';
-import Booking from '../Customer/Booking';
-import Payment from '../Customer/Payment';
 import Registration from '../Customer/Registration';
-import SaveRoom from '../Customer/SaveRoom';
 import ConfirmationPage from '../Customer/ConfirmationPage'
 import CustomerHome from '../Customer/CustomerHome'
 import WorkerMenu from '../Workers/WorkerMenu'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import EditTasks from '../Workers/EditTasks';
-import Employees from '../Workers/Employees';
- import ManualCheckIn from '../Workers/ManualCheckIn';
- import CheckOut from '../Workers/CheckOut';
- 
+import Shift from '../Workers/Shift';
+import AddCharge from '../Workers/AddCharge'
+import ManualCheckIn from '../Workers/ManualCheckIn';
+import EmployeesManagement from '../Workers/EmployeesManagement';
+import UpdateDetails from '../Workers/UpdateDetails';
 
 const Stack = createNativeStackNavigator();
 
 
 export default function Router() {
-    return (
+  return (
     <NavigationContainer independent={true}>
       <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-      <Stack.Screen name="Drawer" component={Drawer} options={{ headerShown: false }} />
-      <Stack.Screen name="Tasks" component={Tasks} options={{ headerShown: false }} />
-      <Stack.Screen name="WorkerMenu" component={WorkerMenu} options={{ headerShown: false}} />
-      <Stack.Screen name="CheckOut" component={CheckOut} options={{ headerShown: false}} />
-      
-      {/* <Stack.Screen name="Booking" component={Booking} options={{title : " "}}/> */}
-      {/* <Stack.Screen name="Payment" component={Payment}options={{title : " "}} /> */}
-      <Stack.Screen name="CustomerHome" component={CustomerHome}options={{title : " "}} />
-      <Stack.Screen name="Registration" component={Registration} options={{title : " "}}/>
-      {/* <Stack.Screen name="SaveRoom" component={SaveRoom}options={{title : " "}} /> */}
-      <Stack.Screen name="ConfirmationPage" component={ConfirmationPage} options={{title : " "}}/>
-      <Stack.Screen name="EditTasks" component={EditTasks} options={{ headerShown: false }} />
-      <Stack.Screen name="Employees" component={Employees} options={{ headerShown: false }} />
-      {/* <Stack.Screen name="CheckIn" component={CheckIn} options={{ headerShown: false }} /> */}
-   <Stack.Screen name="ManualCheckIn" component={ManualCheckIn} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+        <Stack.Screen name="Drawer" component={Drawer} options={{ headerShown: false }} />
+        <Stack.Screen name="Tasks" component={Tasks} options={{ headerShown: false }} />
+        <Stack.Screen name="WorkerMenu" component={WorkerMenu} options={{ headerShown: false }} />
+        <Stack.Screen name="CustomerHome" component={CustomerHome} options={{ title: " " }} />
+        <Stack.Screen name="Registration" component={Registration} options={{ title: " " }} />
+        <Stack.Screen name="ConfirmationPage" component={ConfirmationPage} options={{ title: " " }} />
+        <Stack.Screen name="EditTasks" component={EditTasks} options={{ headerShown: false }} />
+        <Stack.Screen name="Shift" component={Shift} options={{ headerShown: false }} />
+        <Stack.Screen name="AddCharge" component={AddCharge} options={{ title: " " }} />
+        <Stack.Screen name="ManualCheckIn" component={ManualCheckIn} options={{ title: " " }} />
+        <Stack.Screen name="EmployeesManagement" component={EmployeesManagement} options={{ title: " " }} />
+        <Stack.Screen name="UpdateDetails" component={UpdateDetails} options={{ title: " " }} />
+
       </Stack.Navigator>
-      </NavigationContainer>
-      
-    )
+    </NavigationContainer>
+
+  )
 }
