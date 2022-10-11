d8b28e6a9fdf32d23e820ac3e66a1f1756987e22
Irit41 <81565397+Irit41@users.noreply.github.com>
2022-10-11 12:41:19 +0300

    Update ShortCheckIn.jsx

--- LHOTEL_APP/Pages/Workers/ShortCheckIn.jsx
+++ LHOTEL_APP/Pages/Workers/ShortCheckIn.jsx
@@ -317,7 +317,11 @@ const DeleteReservation = async () =>{
     const requestOptions = {
         method: 'DELETE',
         body: JSON.stringify({
+<<<<<<< Updated upstream
             id:roomsReservation.CustomerID
+=======
+            id:id
+>>>>>>> Stashed changes
         }),
         headers: { 'Content-Type': 'application/json' }
     };
